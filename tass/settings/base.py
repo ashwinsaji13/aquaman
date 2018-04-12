764bbc840f03cd8ddde33c110b2efcbacaceb3e5
rdt_ashwinsaji <aswin@rawdatatech.com>
2018-04-12 13:04:17 +0530

    modified base settings adding cors to setting

--- tass/settings/base.py
+++ tass/settings/base.py
@@ -145,9 +145,10 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 25
 }
 
+CORS_ORIGIN_ALLOW_ALL = True
 
 # settings to send email
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
